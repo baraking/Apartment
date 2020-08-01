2f90db6b0afff3e93121339dd2569ac6d978a4a3
baraking <barakatz.bk@gmail.com>
2020-08-01 19:17:09 +0300

    HomeWork 1 complete

---
+++ Assets/cameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    [Range(0.1f,3)]
+    [SerializeField]private float cameraSpeed=1f;
+
+    [Range(0.01f, 45)]
+    [SerializeField] private float angularSpeed = 0.1f;
+
+    private float rotationAngle;
+    void Start()
+    {
+        //cameraSpeed = 1f;
+        rotationAngle = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.Translate(Vector3.forward* cameraSpeed*Time.deltaTime);
+
+        rotationAngle += mouseX * angularSpeed;
+
+        transform.Rotate(new Vector3(0,rotationAngle));
+    }
+}
