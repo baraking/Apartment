a8b450a72c2f65facbe823c6def5958ea42ee6bf
baraking <barakatz.bk@gmail.com>
2020-08-14 18:03:14 +0300

    Door Animation is ready. Need to apply to all

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    [SerializeField] public GameObject gameObject;
+
+    public Transform doorAxis;
+
+    void Start()
+    {
+        if (doorAxis == null)
+            doorAxis = this.transform.GetChild(0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.Play("LeftDoorOpen");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.Play("LeftDoorClose");
+    }
+}
