8da11492937399c434a6917451279c4872044578
baraking <barakatz.bk@gmail.com>
2020-08-21 10:01:36 +0300

    Added TV functionality

---
+++ Assets/Scripts/TV.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV : MonoBehaviour
+{
+    [SerializeField] public GameObject tvScreen;
+
+    private void Start()
+    {
+        tvScreen.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        tvScreen.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        tvScreen.SetActive(false);
+    }
+}
